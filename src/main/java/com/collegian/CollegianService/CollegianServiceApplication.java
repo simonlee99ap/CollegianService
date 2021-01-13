3de9f84255ed0470aadcf4212857f44af5027039
Simon Lee <simonbest45@gmail.com>
2021-01-12 19:27:46 -0500

    initialized with spring initializer

---
+++ src/main/java/com/collegian/CollegianService/CollegianServiceApplication.java
@@ -0,0 +1,13 @@
+package com.collegian.CollegianService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegianServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegianServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collegian/CollegianService/CollegianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegian.CollegianService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegianServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
